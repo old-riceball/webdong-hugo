8efc855b8eb19af4893d16edd495ce7e3637c741
Riceball <42161586+Anderice@users.noreply.github.com>
2022-08-02 10:49:53 +0800

    Update index matter

--- assets/js/index/matter.js
+++ assets/js/index/matter.js
@@ -132,212 +132,35 @@ setWall()
 setBodies()
 
 function setBodies() {
-    // object colors & variables
-    var radius = 20
-
-    // create objects
-    var dongA = Bodies.trapezoid(100, 100, 200, 200, 2, {
-        chamfer: { radius: [10, 10, 10] },
-    })
-    var dongB = Bodies.trapezoid(100, 100, 200, 200, 2, {
-        chamfer: { radius: [10, 10, 10] },
-    })
-    var dongC = Bodies.trapezoid(100, 100, 200, 200, 2, {
-        chamfer: { radius: [10, 10, 10] },
-    })
-
-    // art & design
-    var illustration = Bodies.rectangle(140, 500, 133, 40, {
-        chamfer: { radius: radius },
+    const react = Bodies.rectangle(100, 100, 200, 200, {
         render: {
             sprite: {
-                texture: 'https://i.imgur.com/RADmiFI.png',
-                xScale: 0.5,
-                yScale: 0.5,
-            },
-        },
-    })
-    var art = Bodies.rectangle(35, 460, 56, 40, {
-        chamfer: { radius: radius },
-        render: {
-            sprite: {
-                texture: 'https://i.imgur.com/NwQqeng.png',
-                xScale: 0.5,
-                yScale: 0.5,
-            },
-        },
-    })
-    var threeD = Bodies.rectangle(90, 460, 52, 40, {
-        chamfer: { radius: radius },
-        render: {
-            sprite: {
-                texture: 'https://i.imgur.com/ptUWXgO.png',
-                xScale: 0.5,
-                yScale: 0.5,
-            },
-        },
-    })
-    var graphic = Bodies.rectangle(60, 420, 105, 40, {
-        chamfer: { radius: radius },
-        render: {
-            sprite: {
-                texture: 'https://i.imgur.com/TyOmVtt.png',
-                xScale: 0.5,
-                yScale: 0.5,
-            },
-        },
-    })
-    var photo = Bodies.rectangle(50, 380, 86, 40, {
-        chamfer: { radius: radius },
-        render: {
-            sprite: {
-                texture: 'https://i.imgur.com/tc3MsJP.png',
-                xScale: 0.5,
-                yScale: 0.5,
-            },
-        },
-    })
-    // video
-    var documentary = Bodies.rectangle(220, 540, 165, 40, {
-        chamfer: { radius: radius },
-        render: {
-            sprite: {
-                texture: 'https://i.imgur.com/QYNTBNr.png',
-                xScale: 0.5,
-                yScale: 0.5,
-            },
-        },
-    })
-    var animation = Bodies.rectangle(200, 490, 128, 40, {
-        chamfer: { radius: radius },
-        render: {
-            sprite: {
-                texture: 'https://i.imgur.com/rSnEY9Q.png',
-                xScale: 0.5,
-                yScale: 0.5,
-            },
-        },
-    })
-    var vintage = Bodies.rectangle(190, 440, 104, 40, {
-        chamfer: { radius: radius },
-        render: {
-            sprite: {
-                texture: 'https://i.imgur.com/5BSBvSm.png',
-                xScale: 0.5,
-                yScale: 0.5,
-            },
-        },
-    })
-    var short = Bodies.rectangle(170, 390, 82, 40, {
-        chamfer: { radius: radius },
-        render: {
-            sprite: {
-                texture: 'https://i.imgur.com/VEyrikN.png',
-                xScale: 0.5,
-                yScale: 0.5,
-            },
-        },
-    })
-    //misc
-    var website = Bodies.rectangle(360, 420, 108, 40, {
-        chamfer: { radius: radius },
-        render: {
-            sprite: {
-                texture: 'https://i.imgur.com/hr9p4uV.png',
-                xScale: 0.5,
-                yScale: 0.5,
-            },
-        },
-    })
-    var article = Bodies.rectangle(300, 380, 92, 40, {
-        chamfer: { radius: radius },
-        render: {
-            sprite: {
-                texture: 'https://i.imgur.com/n6TV7XG.png',
-                xScale: 0.5,
-                yScale: 0.5,
-            },
-        },
-    })
-    var music = Bodies.rectangle(400, 360, 86, 40, {
-        chamfer: { radius: radius },
-        render: {
-            sprite: {
-                texture: 'https://i.imgur.com/dax8MwT.png',
-                xScale: 0.5,
-                yScale: 0.5,
-            },
-        },
-    })
-    var star = Bodies.rectangle(80, 260, 42, 40, {
-        chamfer: { radius: radius },
-        render: {
-            sprite: {
-                texture: 'https://i.imgur.com/C2qPMbB.png',
-                xScale: 0.5,
-                yScale: 0.5,
-            },
-        },
-    })
-    //about
-    var about = Bodies.rectangle(230, 140, 87, 40, {
-        chamfer: { radius: radius },
-        render: {
-            sprite: {
-                texture: 'https://i.imgur.com/4gPcZVN.png',
-                xScale: 0.5,
-                yScale: 0.5,
-            },
-        },
-    })
-    var instagram = Bodies.rectangle(320, 180, 40, 40, {
-        id: 'instagramBody',
-        chamfer: { radius: radius },
-        render: {
-            sprite: {
-                texture: 'https://i.imgur.com/RStSwfG.png',
-                xScale: 0.5,
-                yScale: 0.5,
-            },
-        },
-        url: '{{ $.Site.Params.brandInstagram }}',
-    })
-    var random = Bodies.rectangle(230, 180, 112, 40, {
-        chamfer: { radius: radius },
-        render: {
-            sprite: {
-                texture: 'https://i.imgur.com/YS51eIC.png',
-                xScale: 0.5,
-                yScale: 0.5,
+                texture:
+                    'https://res.cloudinary.com/webdong/image/upload/v1659408295/global/react.png',
             },
         },
     })
 
-    // Original Shape
-    // var illustration = Bodies.rectangle(70, 500, 133, 40, {render: { fillStyle: arts}, chamfer: {radius: 20}})
+    const vue = Bodies.rectangle(300, 100, 200, 200, {
+        render: {
+            sprite: {
+                texture:
+                    'https://res.cloudinary.com/webdong/image/upload/v1659408295/global/vue.png',
+            },
+        },
+    })
+
+    const working = Bodies.rectangle(500, 100, 200, 200, {
+        render: {
+            sprite: {
+                texture:
+                    'https://res.cloudinary.com/webdong/image/upload/v1659408529/global/working.png',
+            },
+        },
+    })
 
     // add all of the bodies to the world
-    World.add(engine.world, [
-        illustration,
-        art,
-        threeD,
-        graphic,
-        photo,
-        documentary,
-        animation,
-        vintage,
-        short,
-        website,
-        article,
-        music,
-        star,
-        about,
-        instagram,
-        random,
-        dongA,
-        dongB,
-        dongC,
-    ])
+    World.add(engine.world, [react, vue, working])
 
     // add mouse control
     var mouse = Mouse.create(render.canvas),
